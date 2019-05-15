1492be9efc05dfc6b6fe3f81bdf372397a090d2d
SamuraiSusy <susyy-@hotmail.com>
2019-05-15 13:37:04 +0300

    started to make a mobile version so created indexnew and stylenew. nav bar's shrinking working.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
+let slideIndex = 0;
+showSlides();
 
-        let slideIndex = 0;
-        showSlides();
-        /* tee vaakatason kuvansiirto */
-        function showSlides() {
-            let slides = document.getElementsByClassName("pimgs");
-            for (let i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none"; 
-            }
+/* tee vaakatason kuvansiirto */
+function showSlides() {
+    let slides = document.getElementsByClassName("pimgs");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
 
-            slideIndex++;
+    slideIndex++;
 
-            if (slideIndex > slides.length) {slideIndex = 1} 
+    if (slideIndex > slides.length) {slideIndex = 1} 
 
-            slides[slideIndex-1].style.display = "block"; 
-            
-            setTimeout(showSlides, 8000); // Change image every 2 seconds
-            }
+    slides[slideIndex-1].style.display = "block"; 
+    
+    setTimeout(showSlides, 8000); // Change image every 2 seconds
+}
+
+let toggleNavStatus = false;
+
+let toggleNav = function() {
+    let getNav;
+}
+
+// cant change the hight/width of an element, can change padding
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("hedr").style.padding = "0px 10px";
+    document.getElementById("logo").style.fontSize = "22px";
+  } else {
+    document.getElementById("hedr").style.padding = "15px 10px";
+    document.getElementById("logo").style.fontSize = "28px";
+  }
+}
