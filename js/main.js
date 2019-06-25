22b3367fc103a5b28ea4677a27e9ee1bdd27fb93
SamuraiSusy <susyy-@hotmail.com>
2019-06-26 01:25:02 +0300

    made the link bar shrinkable and functional. started flexbox for main content.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
+let slideIndex = 0;
+showSlides();
+/* tee vaakatason kuvansiirto */
+function showSlides() {
+    let slides = document.getElementsByClassName("pimgs");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
 
-        let slideIndex = 0;
-        showSlides();
-        /* tee vaakatason kuvansiirto */
-        function showSlides() {
-            let slides = document.getElementsByClassName("pimgs");
-            for (let i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none"; 
-            }
+    slideIndex++;
 
-            slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
 
-            if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block";
 
-            slides[slideIndex-1].style.display = "block"; 
-            
-            setTimeout(showSlides, 8000); // Change image every 2 seconds
-            }
+    setTimeout(showSlides, 8000); // Change image every 2 seconds
+}
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("hdr").style.padding = "0px 20px";
+    document.getElementById("hdr").style.fontSize = "24px"
+  } else {
+    document.getElementById("hdr").style.padding = "20px";
+    document.getElementById("hdr").style.fontSize = "30px"
+  }
+}
